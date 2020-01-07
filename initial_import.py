ef91613dc6208468b7157fa851d2a9ae7210603f
Michael Rice <MichaelRi@sellen.com>
2020-01-07 15:18:26 -0800

    Better verbosity and readability of initial import

--- initial_import.py
+++ initial_import.py
@@ -1,8 +1,18 @@
 from openpyxl import load_workbook
 from okta_api import API
+from enum import Enum
 
 okta = API()
 source = list(load_workbook('OKTA Query from Snowflake.xlsx', read_only=True).worksheets[0].rows)
 
 for row in range(1, len(source)):
-    okta.CreateUser(source[row][2].value, source[row][1].value, source[row][3].value, source[row][4].value, source[row][0].value)
+    First = source[row][2].value
+    Last = source[row][1].value
+    Email = source[row][3].value
+    Phone = source[row][4].value
+    Eid = source[row][0].value
+
+    Phone = Phone if Phone is not None else ''
+
+    okta.CreateUser(First, Last, Email, Phone, Eid)
+    print("Created  {}: {} {} - {} - {}".format(Eid, First, Last, Email, Phone))
