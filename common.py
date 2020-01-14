6b3c8c4d0ed464a8abf577acd7be3a83abc16fd8
Michael Rice <MichaelRi@sellen.com>
2020-01-14 10:29:42 -0800

    Full support for snowflake connection

--- common.py
+++ common.py
@@ -4,11 +4,16 @@ class Common:
     def __init__(self):
         self.okta_key = ''
         self.snowflake_key = ''
+        self.snowflake_user = ''
+        self.snowflake_account = ''
+        self.snowflake_warehouse = ''
+        self.snowflake_database = ''
+        self.snowflake_schema = ''
         parser = configparser.ConfigParser()
 
         if not os.path.isfile('config.txt'):
             configFile = open('config.txt', 'w+')
-            configFile.write('[Okta]\nkey = \n\n[Snowflake]\nkey = ')
+            configFile.write('[Okta]\nkey = \n\n[Snowflake]\nkey = \nuser = \naccount = \nwarehouse = \ndatabase = \nschema = ')
             configFile.close()
             print('A new config.txt file has been created. Please enter your API key into this file')
             sys.exit()
@@ -21,8 +26,13 @@ class Common:
             print('config.txt is not properly formatted')
             sys.exit()
 
-        if 'Snowflake' in parser and 'key' in parser['Snowflake']:
+        if 'Snowflake' in parser and all(s in parser['Snowflake'] for s in ('key', 'user', 'account', 'warehouse', 'database', 'schema')):
             self.snowflake_key = parser['Snowflake']['key']
+            self.snowflake_user = parser['Snowflake']['user']
+            self.snowflake_account = parser['Snowflake']['account']
+            self.snowflake_warehouse = parser['Snowflake']['warehouse']
+            self.snowflake_database = parser['Snowflake']['database']
+            self.snowflake_schema = parser['Snowflake']['schema']
         else:
             print('config.txt is not properly formatted')
             sys.exit()
